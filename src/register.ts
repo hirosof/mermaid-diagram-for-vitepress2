bddd6a0a8a3233db9b6ec1474eb53dce966c9bab
hirosof <for-git@mail.hirosof.dev>
2026-03-01 03:51:04 +0900

    コンポーネントの利用先でCSSが反映できない問題への対応と、エクスポート設定や一部関数名の変更を実施

--- src/register.ts
+++ src/register.ts
@@ -2,7 +2,7 @@ import MermaidDiagramRenderer from './components/MermaidDiagramRenderer.vue'
 
 import type { EnhanceAppContext } from 'vitepress'
 
-export function configureMDRTheme({ app }: EnhanceAppContext) {
+export function registerMermaidDiagramRendererComponent({ app }: EnhanceAppContext) {
   app.component('MermaidDiagramRenderer', MermaidDiagramRenderer)
 }
 
